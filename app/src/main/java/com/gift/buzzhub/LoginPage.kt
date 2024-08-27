9fb1a42ce7856dbee7740b7d34b4d457d06f04ad
gift <www.x.dlamini17@gmail.com>
2024-08-27 23:00:56 +0200

    without gambling

--- app/src/main/java/com/gift/buzzhub/LoginPage.kt
+++ app/src/main/java/com/gift/buzzhub/LoginPage.kt
@@ -70,6 +70,7 @@ class LoginPage : AppCompatActivity() {
                         Toast.LENGTH_SHORT).show()
                     val intent = Intent(this@LoginPage,HomePage::class.java)
                     startActivity(intent)
+                    finish()
 
                 } else {
                     // If sign in fails, display a message to the user, specifically an exception.

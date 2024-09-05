d230848821c2e7d2117a235c9ad639aa5b944819
Matimba98 <matimbalubisi98@gmail.com>
2024-09-05 22:50:48 +0200

    settings page not fully complete.

--- app/src/main/java/com/gift/buzzhub/AccountPage.kt
+++ app/src/main/java/com/gift/buzzhub/AccountPage.kt
@@ -8,37 +8,32 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class SettingsPage : AppCompatActivity() {
+class AccountPage : AppCompatActivity() {
 
     lateinit var recyclerView: RecyclerView
-    var settingsList = ArrayList<String>()
-    lateinit var adapter: SettingsAdapter
-
+    var accountList = ArrayList<String>()
+    lateinit var adapter: AccountAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_settings_page)
+        setContentView(R.layout.activity_account)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
-        recyclerView = findViewById(R.id.settings_recyclerView)
+        recyclerView = findViewById(R.id.account_recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
+        accountList.add("Update name")
+        accountList.add("Update email")
+        accountList.add("Update contact number")
+        accountList.add("Update profile")
+        accountList.add("Two factor authentication")
 
-        settingsList.add("Account")
-        settingsList.add("Notification")
-        settingsList.add("Payment methods")
-        settingsList.add("Display")
-        settingsList.add("Help centre")
-        settingsList.add("Privacy policy")
-        settingsList.add("Delete account")
-
-        adapter = SettingsAdapter(settingsList, this)
+        adapter = AccountAdapter(this, accountList)
         recyclerView.adapter = adapter
 
 
